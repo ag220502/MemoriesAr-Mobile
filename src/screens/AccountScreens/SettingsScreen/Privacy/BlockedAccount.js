13529a95a08e8b9e6ddacedc0fe05ff7c2d36895
ag220502 <71845758+ag220502@users.noreply.github.com>
2023-02-24 12:12:05 +0400

    blocked accounts

--- src/screens/AccountScreens/SettingsScreen/Privacy/BlockedAccount.js
+++ src/screens/AccountScreens/SettingsScreen/Privacy/BlockedAccount.js
@@ -6,8 +6,9 @@ import { Entypo } from '@expo/vector-icons';
 import Color from './../../../../ColourThemes/theme1.js'
 
 
-const BlockedAccount = ({navigation}) => {
-  let chat = [
+const BlockedAccount = ({navigation,route}) => {
+
+  	let chat = [
     {
         id:1,
         username:'Akshay',
@@ -34,70 +35,109 @@ const BlockedAccount = ({navigation}) => {
         profile_image:require('../../../../images/HomeImages/profile.png')
     },
 
-]
-  const [keyword,setKeyword] = useState('')
-    const [showbar,setShowBar] = useState(false)
-  return (
-    <View style={styles.container}>
-        <StatusBar barStyle={"light-content"}/>
-        <View style={styles.search}>
-            <Pressable style={styles.buttonView}
-                onPress={()=>navigation.navigate("Settings")}
-            >
-                <Ionicons name="chevron-back" size={30} color={Color.textDarkColor} />
-            </Pressable>
-            <View style={styles.headView}>
-                {showbar ? 
-                    <TextInput style={styles.searchInput} placeholder="Search" onChangeText={(text)=>setKeyword(text)}/>
-                    :
-                    <Text style={styles.head}>Blocked Users</Text>}
-                
-            </View>
-            {showbar ? 
-                <Pressable style={styles.buttonView} onPress={()=>{setShowBar(false);setKeyword('')}}>
-                    <Entypo name="cross" size={30} color={Color.textDarkColor} />
-                </Pressable>: 
-                <Pressable style={styles.buttonView} onPress={()=>setShowBar(true)}>
-                    <FontAwesome name="search" size={24} color={Color.textDarkColor}  />
-                </Pressable>}
-        </View>
-        <ScrollView style={styles.main}>
-            {
-                chat.filter((data)=>{
-                    if(data.username.toLowerCase().includes(keyword.toLowerCase())){
-                        return data
-                    }
-                    else if(keyword===''){
-                        return data
-                    }
-                }).map((data,index)=>{
-                    return(
-                      <Pressable key={index} style={styles.containerUser} onPress={
-                        ()=>{
-                          navigation.navigate("OtherUserProfileScreen")
-                        }
-                    }>
-                      <View style={styles.user_det}>
-                        <View>
-                            <Image source={data.profile_image} style={styles.profile_img}/>
-                        </View>
-                        <View>
-                            <Text style={styles.user_name}>{data.username}
-                            </Text>
-                        </View>
-                      </View>
+	]
+	const [keyword,setKeyword] = useState('')
+	const [showbar,setShowBar] = useState(false)
+	const [user,setUser] = useState([])
+	const [getData,setGetData] = useState(false)
+	const getUserData= async()=>{
+		try{
+			await fetch ('http://localhost:3000/api/block/allBlockUser/'+route.params.userId,{
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				}
+			}).then(
+				res => {
+					if(res.status===200)
+					{
+						res.json().then((data)=>{
+							console.log(data)
+							if(data[0])
+							{
+								setUser(data)
+							}
+						})
+					}
+					else if(res.status==404)
+					{
+						console.log("Res"+res)
+					}
+			})
+		}
+		catch(err)
+		{
+			console.log(err)
+		}
+	}
+	if(!getData)
+	{
+		getUserData()
+		setGetData(true)
+	}	
 
-                      <View style={styles.user_det}>
-                      <Entypo name="dots-three-horizontal" size={24} color="black" />
-                      </View>
-                    </Pressable>
-                    )
-                })
-            }
-            
-        </ScrollView>
-    </View>
-  )
+
+	return (
+		<View style={styles.container}>
+			<StatusBar barStyle={"light-content"}/>
+			<View style={styles.search}>
+				<Pressable style={styles.buttonView}
+					onPress={()=>navigation.navigate("Settings",{userId:route.params.userId})}
+				>
+					<Ionicons name="chevron-back" size={30} color={Color.textDarkColor} />
+				</Pressable>
+				<View style={styles.headView}>
+					{showbar ? 
+						<TextInput style={styles.searchInput} placeholder="Search" onChangeText={(text)=>setKeyword(text)}/>
+						:
+						<Text style={styles.head}>Blocked Users</Text>}
+					
+				</View>
+				{showbar ? 
+					<Pressable style={styles.buttonView} onPress={()=>{setShowBar(false);setKeyword('')}}>
+						<Entypo name="cross" size={30} color={Color.textDarkColor} />
+					</Pressable>: 
+					<Pressable style={styles.buttonView} onPress={()=>setShowBar(true)}>
+						<FontAwesome name="search" size={24} color={Color.textDarkColor}  />
+					</Pressable>}
+			</View>
+			<ScrollView style={styles.main}>
+				{
+					chat.filter((data)=>{
+						if(data.username.toLowerCase().includes(keyword.toLowerCase())){
+							return data
+						}
+						else if(keyword===''){
+							return data
+						}
+					}).map((data,index)=>{
+						return(
+						<Pressable key={index} style={styles.containerUser} onPress={
+							()=>{
+							navigation.navigate("OtherUserProfileScreen")
+							}
+						}>
+						<View style={styles.user_det}>
+							<View>
+								<Image source={data.profile_image} style={styles.profile_img}/>
+							</View>
+							<View>
+								<Text style={styles.user_name}>{data.username}
+								</Text>
+							</View>
+						</View>
+
+						<View style={styles.user_det}>
+						<Entypo name="dots-three-horizontal" size={24} color="black" />
+						</View>
+						</Pressable>
+						)
+					})
+				}
+				
+			</ScrollView>
+		</View>
+	)
 }
 
 export default BlockedAccount
