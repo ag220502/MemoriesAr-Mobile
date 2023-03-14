2cf4157e79576b32433de19c25bd538c303d7b4b
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-14 17:21:56 +0400

    fetching data

---
+++ src/screens/fetchData/viewPost.js
@@ -0,0 +1,33 @@
+const viewPostById = async (id) => {
+  const response = await fetch(
+    "http://localhost:3000/api/posts/getPostById/" + id
+  );
+  const data = await response.json();
+  return data;
+};
+
+const checkLiked = async (postId, userId) => {
+  const response = await fetch(
+    "http://localhost:3000/api/posts/checkLiked/" + postId + "/" + userId
+  );
+  const data = await response.json();
+  return data;
+};
+
+const checkDisliked = async (postId, userId) => {
+  const response = await fetch(
+    "http://localhost:3000/api/posts/checkDisliked/" + postId + "/" + userId
+  );
+  const data = await response.json();
+  return data;
+};
+
+const checkSaved = async (postId, userId) => {
+  const response = await fetch(
+    "http://localhost:3000/api/posts/checkSaved/" + postId + "/" + userId
+  );
+  const data = await response.json();
+  return data;
+};
+
+module.exports = { viewPostById, checkLiked, checkDisliked, checkSaved };
