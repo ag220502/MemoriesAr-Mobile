18a59cb386f2f3f2a0f56f8c4ee9ccf7ee791a57
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-19 15:36:58 +0400

    fetch

--- src/screens/fetchData/createPost.js
+++ src/screens/fetchData/createPost.js
@@ -1,8 +1,7 @@
-import {WEB} from "../../../var.js"
+import { WEB } from "../../../var.js";
 // import {uploadImagex} from "../../../firebase/functions/index.js"r
 
 const createPost = async (
-
   userId,
   caption,
   lattitude,
@@ -11,41 +10,30 @@ const createPost = async (
   postImage,
   tag
 ) => {
-  console.log("Creating Post")
-  console.log("Image in func is "+postImage)
-
-  // console.log("Creating Post")
-  // const formData = new FormData();
-  // formData.append("userId", userId);
-  // formData.append("caption", caption);
-  // formData.append("lattitude", lattitude);
-  // formData.append("longitude", longitude);
-  // formData.append("flag", flag);
-  // formData.append("postImage", postImage);
-  // formData.append("tag", tag);
-  const response = await fetch(WEB+"/api/posts/create", {
+  console.log("Creating Post");
+  console.log("Image in func is " + postImage);
+  const response = await fetch(WEB + "/api/posts/create", {
     method: "POST",
     headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify({
-        userId: userId,
-        caption: caption,
-        lattitude: lattitude,
-        longitude: longitude,
-        flag: flag,
-        postImage: postImage,
-        tag: tag
-    })
+      userId: userId,
+      caption: caption,
+      lattitude: lattitude,
+      longitude: longitude,
+      flag: flag,
+      postImage: postImage,
+      tag: tag,
+    }),
   });
-  
-  console.log("Response is ")
-  console.log(response)
-  // const data = await response.json();
-  // console.log(data)
-  // return data;
 
+  console.log("Response is ");
+  console.log(response);
+  const data = await response.json();
+  // console.log(data)
+  return data;
 };
 
 module.exports = { createPost };
--- src/screens/fetchData/searchData.js
+++ src/screens/fetchData/searchData.js
@@ -1,21 +1,41 @@
-import {WEB} from "../../../var.js"
+import { WEB } from "../../../var.js";
 const searchUser = async (search) => {
-    const response = await fetch(WEB+"/api/search/byName/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(WEB + "/api/search/byName/" + search);
+  const data = await response.json();
+  return data;
+};
 
 const recentSearch = async (search) => {
-    const response = await fetch(WEB+"/api/recentSearches/userRecentSearches/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(
+    WEB + "/api/recentSearches/userRecentSearches/" + search
+  );
+  const data = await response.json();
+  return data;
+};
 
 const suggetUser = async (search) => {
-    const response = await fetch(WEB+"/api/recentSearches/suggestedUsers/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(
+    WEB + "/api/recentSearches/suggestedUsers/" + search
+  );
+  const data = await response.json();
+  return data;
+};
 
+const createRecentSearch = async (userId, searchedUserId, statusTime) => {
+  const response = await fetch(WEB + "/api/recentSearches/createSearch", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: userId,
+      searchedUserId: searchedUserId,
+      statusTime: statusTime,
+    }),
+  });
+  const data = await response.json();
+  return data;
+};
 
-module.exports = {searchUser,recentSearch,suggetUser}
+module.exports = { searchUser, recentSearch, suggetUser, createRecentSearch };
