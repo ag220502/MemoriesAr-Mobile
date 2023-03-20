38875d766376b9156de4aebc253c52007d3c15c7
Dhruv Chaturvedi <dc2020@hw.ac.uk>
2023-03-20 17:16:51 +0400

    like and comment scrapbooks

--- src/screens/fetchData/scrapbookUtils.js
+++ src/screens/fetchData/scrapbookUtils.js
@@ -1,5 +1,127 @@
 import {WEB} from "../../../var.js"
 
+// scrapbooks comments related
 
+const addComment = async (scrapId, userId, comment) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/addComment", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId : userId,
+            comment : comment,
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
 
-module.exports = {} 
+const deleteComment = async (commentId, userId, scrapId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/deleteComment", {
+        method : "DELETE",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            commentId : commentId,
+            userId : userId,
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getComments = async (scrapId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/getComments", {
+        method : "GET",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+// scrapbook Likes related
+
+const addLike = async (scrapId, userId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/addLike", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId : userId,
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getScrapLikes = async (scrapId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/getScrapLikes", {
+        method : "GET",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getUserLikes = async (userId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/getUserLikes", {
+        method : "GET",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            userId : userId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+const unLike = async (scrapId, userId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/unLike", {
+        method : "DELETE",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            userId : userId,
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+const checkLike = async (scrapId, userId) =>{
+    const response = await fetch(WEB+"/api/scrapUtils/checkLike", {
+        method : "GET",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            userId : userId,
+            scrapId : scrapId
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+module.exports = { addComment, deleteComment, getComments,
+                   addLike, getScrapLikes, getUserLikes, unLike, checkLike } 
