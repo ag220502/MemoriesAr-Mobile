014a6f6bd3d168fdbf2cb989a23de10de2574cd8
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-20 14:44:06 +0400

    deletesearch

--- src/screens/fetchData/searchData.js
+++ src/screens/fetchData/searchData.js
@@ -1,21 +1,37 @@
-import {WEB} from "../../../var.js"
+import { WEB } from "../../../var.js";
 const searchUser = async (search) => {
-    const response = await fetch(WEB+"/api/search/byName/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(WEB + "/api/search/byName/" + search);
+  const data = await response.json();
+  return data;
+};
 
 const recentSearch = async (search) => {
-    const response = await fetch(WEB+"/api/recentSearches/userRecentSearches/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(
+    WEB + "/api/recentSearches/userRecentSearches/" + search
+  );
+  const data = await response.json();
+  return data;
+};
 
 const suggetUser = async (search) => {
-    const response = await fetch(WEB+"/api/recentSearches/suggestedUsers/" + search);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(
+    WEB + "/api/recentSearches/suggestedUsers/" + search
+  );
+  const data = await response.json();
+  return data;
+};
 
+const deleteSearch = async (userId, searchedUserId) => {
+  const response = await fetch(WEB + "/api/recentSearches/deleteSearch/", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId,
+      searchedUserId,
+    }),
+  });
+};
 
-module.exports = {searchUser,recentSearch,suggetUser}
+module.exports = { searchUser, recentSearch, suggetUser, deleteSearch };
